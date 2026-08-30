811a7df6b7d67933e39943f96c63e86b867ed75a
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-30 16:50:22 +0000

    Exemplo

--- 05-heranca.js
+++ 05-heranca.js
@@ -12,9 +12,25 @@ class Animal {
     }
 } 
 
+class Cachorro extends Animal {
+    constructor(nome) {
+        super(nome);
+        console.log(this.nome + ': latido de cachorro.');
+    }
+}
+
 let a = new Animal("Animal qualquer");
 a.som();
-a.comida("Ração"); a.setNome = "Rufos";
+a.comida("Ração"); 
+a.setNome = "Rufos";
 console.log(`Nome: ${a.getNome}`);
 a.som();
 a.comida("Leite");
+
+let b = new Animal("Cosmos");
+b.som();
+b.comida("Ração"); 
+b.setNome = "Gordom";
+console.log(`Nome: ${b.getNome}`);
+b.som();
+b.comida("Leite");
